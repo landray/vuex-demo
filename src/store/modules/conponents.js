bcd7b2e73b2ebf123288dc88a84c144e74015048
Saiya <evecalm@live.com>
2017-01-03 11:27:18 +0800

    添加缺少的配置文件

--- src/store/modules/conponents.js
+++ src/store/modules/conponents.js
@@ -1,5 +1,12 @@
 import * as localstore from '../localstore'
 
+// components = [{
+//   name: 'home',
+//   id: 'home-xxxxxx',
+//   configData: {
+    
+//   }
+// }]
 const state = {
   // 当前正在编辑的component的配置
   // {name, id, configData}
@@ -16,17 +23,22 @@ const getters = {
 
 const mutations = {
   // component => {id, name}
-  addComponent(state, componentName) {
+  addComponent(state, componentName, pos) {
     const component = {
       name: componentName,
       id: componentName + '-' + (new Date()),
+      configData: {}
     };
     state.components.push(component);
     state.currentComponent = component;
   },
+  deleteComponent (state, componentId) {
+    var cmpt = state.components.find(item => item.id === componentId)
+    state.components.$remove(cmpt);
+  },
   changeCurrentComponent(state, componentId) {
     if (componentId) {
-      var cmpt = state.components.find( item => item.id === componentId );
+      // var cmpt = state.components.find( item => item.id === componentId );
       state.currentComponent = state.components.find(item => item.id === componentId);
     } else {
       state.currentComponent = null;
